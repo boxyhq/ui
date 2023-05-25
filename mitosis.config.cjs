846c6cefcb06ff1337261b0254b01aac8badbf8c
Aswin V <vaswin91@gmail.com>
2023-05-25 23:34:25 +0530

    Add vue options and customise output path for vue

--- mitosis.config.cjs
+++ mitosis.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   files: 'src/**',
   dest: '.',
-  options: { react: { typescript: true } },
-  targets: ['react'],
+  options: { react: { typescript: true }, vue: { typescript: true } },
+  targets: ['react', 'vue'],
+  getTargetPath: ({ target }) => {
+    switch (target) {
+      case 'vue3':
+        return 'vue';
+
+      default:
+        return target;
+    }
+  },
   exclude: ['src/css.d.ts'],
 };
