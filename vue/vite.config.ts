336e1563f8a990d04172307cde190b9813302012
Aswin V <vaswin91@gmail.com>
2023-05-29 17:34:06 +0530

    Inject default styles for css

--- vue/vite.config.ts
+++ vue/vite.config.ts
@@ -3,6 +3,7 @@ import { resolve } from 'node:path';
 
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -29,7 +30,7 @@ export default defineConfig({
       },
     },
   },
-  plugins: [vue()],
+  plugins: [vue(), cssInjectedByJsPlugin()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
