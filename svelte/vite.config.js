96ddc77738e6527e7ea72869a255f0a2fd9844da
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-06-02 14:41:15 +0530

    Resolve alias for package entry point and specify rollupOptions

--- svelte/vite.config.js
+++ svelte/vite.config.js
@@ -1,6 +1,29 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  build: {
+    // use vite library mode to build the package
+    // https://vitejs.dev/guide/build.html#library-mode
+    lib: {
+      entry: resolve(__dirname, 'src/sso/index.ts'),
+      name: 'BoxyHQUI',
+      // the proper extensions will be added
+      fileName: '@boxyhq/svelte-ui',
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      external: ['svelte'],
+      output: {
+        globals: {
+          // Provide global variables to use in the UMD build
+          // for externalized deps
+          svelte: 'Svelte',
+        },
+      },
+    },
+  },
+  plugins: [sveltekit()],
 });
