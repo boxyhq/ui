3e55560b44ff57c26d2ba5ce20f5c01f1cc8e2d6
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-06-02 13:56:33 +0530

    Replace types for node and vars and set typescript to true

--- mitosis.config.cjs
+++ mitosis.config.cjs
@@ -2,7 +2,34 @@
 module.exports = {
   files: 'src/**',
   dest: '.',
-  options: { react: { typescript: true } },
-  targets: ['react'],
+  options: {
+    react: { typescript: true },
+    svelte: {
+      typescript: true,
+      plugins: [
+        () => ({
+          code: {
+            post: (code) => {
+              const updatedCode = [];
+
+              code.split('\n\n').map((el) => {
+                // console.log('----');
+                const x = el.replace('node, vars', 'node: any, vars: any');
+                // console.log('New new element: ', x);
+                // console.log('Old element: ', el);
+                el = x;
+                updatedCode.push(el);
+                // console.log('----');
+              });
+              code = updatedCode.join('\n\n');
+              console.log(updatedCode.join('\n\n'));
+              return code;
+            },
+          },
+        }),
+      ],
+    },
+  },
+  targets: ['react', 'svelte'],
   exclude: ['src/css.d.ts'],
 };
