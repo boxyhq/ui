93c76fefc73ff128ad2f7171ed1e836dcba2b92f
Aswin V <vaswin91@gmail.com>
2023-06-02 10:53:56 +0530

    Doc styling updates

--- vue2/docs/src/components/sso/SSOView.vue
+++ vue2/docs/src/components/sso/SSOView.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
+    <h2>SSO Login Component Usage</h2>
+    <h3 style="border-top: 1px solid #3c3c431f">With Custom Styles</h3>
     <CustomStyling></CustomStyling>
+    <h3>With default Styles</h3>
     <DefaultStyling></DefaultStyling>
+    <h3>Preset SSOIdentifier (No input display)</h3>
     <PresetSSOIdentifier></PresetSSOIdentifier>
+    <h3>With failing onSubmit</h3>
     <FailingSubmit></FailingSubmit>
   </div>
 </template>
