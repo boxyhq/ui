fdac99b11dcdf82a6f2a6093bf401093db9b4764
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-06-04 22:46:59 +0530

    Update components view

--- angular/projects/docs/src/app/components/components.component.ts
+++ angular/projects/docs/src/app/components/components.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { Login } from '@boxyhq/angular-ui';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-components',
   template: `
-    <h1>Hello world welcome to components</h1>
+    <app-navbar></app-navbar>
     <login [inputLabel]="'Team domain*'" [buttonText]="'LOGIN WITH SSO'"></login>
   `,
   standalone: true,
-  imports: [Login],
+  imports: [CommonModule, NavbarComponent, Login],
   styleUrls: ['./components.component.css'],
 })
 export class ComponentsComponent {}
