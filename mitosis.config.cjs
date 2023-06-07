e4c15d3df870c092921ba189bdd51e51c18c692b
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-06-07 10:33:47 +0530

    Clear mitosis options for svelte

--- mitosis.config.cjs
+++ mitosis.config.cjs
@@ -4,31 +4,6 @@ module.exports = {
   dest: '.',
   options: {
     react: { typescript: true },
-    svelte: {
-      typescript: true,
-      plugins: [
-        () => ({
-          code: {
-            post: (code) => {
-              const updatedCode = [];
-
-              // Split mitosis output code using '\n\n'
-              // map through the splitted array and replace every instance of
-              // 'node, vars' with 'node: any, vars: any'
-              code.split('\n\n').map((el) => {
-                const replacedInstance = el.replace('node, vars', 'node: any, vars: any');
-
-                el = replacedInstance;
-                updatedCode.push(el);
-              });
-              code = updatedCode.join('\n\n');
-
-              return code;
-            },
-          },
-        }),
-      ],
-    },
   },
   targets: ['react', 'svelte'],
   exclude: ['src/css.d.ts'],
