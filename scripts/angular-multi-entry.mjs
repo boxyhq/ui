9174c41f909c514eeaeacdbdae7f1ff8e1128246
Aswin V <vaswin91@gmail.com>
2023-06-10 19:19:05 +0530

    Tweak and add comments

--- scripts/angular-multi-entry.mjs
+++ scripts/angular-multi-entry.mjs
@@ -1,8 +1,3 @@
-/** 1. Move each entrypoint folder under angular/projects/boxyhq/angular-ui/tmp/src into
-angular/projects/boxyhq/angular-ui
-
-ex:- tmp/src/sso/* -> ./sso/src
- */
 import cpy from 'cpy';
 import { rimraf } from 'rimraf';
 
@@ -10,8 +5,7 @@ const BASE = `angular/projects/boxyhq/angular-ui`;
 const TMP = `${BASE}/tmp/src`;
 const ENTRYPOINTS = ['sso'];
 
-// Rename all .module.css files
-
+// Rename all .module.css files to .css files
 for (const ep of ENTRYPOINTS) {
   await cpy(`${TMP}/${ep}/**/*.module.css`, `${TMP}/${ep}`, {
     rename: (basename) => basename.replace('.module', ''),
@@ -19,6 +13,8 @@ for (const ep of ENTRYPOINTS) {
   await rimraf(`${TMP}/${ep}/**/*.module.css`, { glob: true });
 }
 
+// Move different entry point source code from mitosis generated bundle
+// into corresponding Angular entry points
 for (const ep of ENTRYPOINTS) {
   await cpy(`${TMP}/${ep}/**`, `${BASE}/${ep}/src`);
 }
