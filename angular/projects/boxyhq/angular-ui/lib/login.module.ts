bfee499a7259b99b78885fd16634277d297ac159
Aswin V <vaswin91@gmail.com>
2023-06-10 19:13:14 +0530

    Refactor lib structure

--- angular/projects/boxyhq/angular-ui/lib/login.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { Login } from './src/sso/login';
-
-@NgModule({
-  declarations: [],
-  imports: [Login],
-  exports: [Login],
-})
-export class AngularUiModule {}
--- angular/projects/boxyhq/angular-ui/lib/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of angular-ui
- */
-
-export * from './login.service';
-export * from './src/sso/login/index';
-export * from './login.module';
