b46df16c36b646b9af38165d07ee641c8c1b4c95
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-07-03 13:04:29 +0530

    Avoid destructing props while defining them with reference to one of mitosis gotchas

--- src/sso/connections/ConnectionList.lite.tsx
+++ src/sso/connections/ConnectionList.lite.tsx
@@ -3,50 +3,39 @@ import PlusIcon from '../../shared/PlusIcon.lite';
 import { LinkPrimary } from '@components/LinkPrimary';
 import { InputWithCopyButton } from '@components/ClipboardButton';
 import { Show } from '@builder.io/mitosis';
+import { ConnectionListProps } from './types';
 
 const DEFAULT_VALUES = {
   isSettingsView: false,
 };
 
-export default function ConnectionList({
-  setupLinkToken,
-  createConnectionUrl,
-  idpEntityID,
-  isSettingsView,
-  translation,
-}: {
-  setupLinkToken?: string;
-  createConnectionUrl: string;
-  idpEntityID?: string;
-  isSettingsView?: boolean;
-  translation: any;
-}) {
+export default function ConnectionList(props: ConnectionListProps) {
   return (
     <div>
       <div className='mb-5 flex items-center justify-between'>
         <h2 className='font-bold text-gray-700 dark:text-white md:text-xl'>
-          {translation(
-            isSettingsView || DEFAULT_VALUES.isSettingsView ? 'admin_portal_sso' : 'enterprise_sso'
+          {props.translation(
+            props.isSettingsView || DEFAULT_VALUES.isSettingsView ? 'admin_portal_sso' : 'enterprise_sso'
           )}
         </h2>
         <div className='flex gap-2'>
-          <LinkPrimary Icon={PlusIcon} href={createConnectionUrl} data-testid='create-connection'>
-            {translation('new_connection')}
+          <LinkPrimary Icon={PlusIcon} href={props.createConnectionUrl} data-testid='create-connection'>
+            {props.translation('new_connection')}
           </LinkPrimary>
-          <Show when={!setupLinkToken && !(isSettingsView || DEFAULT_VALUES.isSettingsView)}>
+          <Show when={!props.setupLinkToken && !(props.isSettingsView || DEFAULT_VALUES.isSettingsView)}>
             <LinkPrimary
               Icon={LinkIcon}
               href='/admin/sso-connection/setup-link/new'
               data-testid='create-setup-link'>
-              {translation('new_setup_link')}
+              {props.translation('new_setup_link')}
             </LinkPrimary>
           </Show>
         </div>
       </div>
-      <Show when={idpEntityID && setupLinkToken}>
+      <Show when={props.idpEntityID && props.setupLinkToken}>
         <div className='mb-5 mt-5 items-center justify-between'>
           <div className='form-control'>
-            <InputWithCopyButton text={idpEntityID} label={translation('idp_entity_id')} />
+            <InputWithCopyButton text={props.idpEntityID} label={props.translation('idp_entity_id')} />
           </div>
         </div>
       </Show>
