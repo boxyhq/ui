0e9d82a0c871cdd8f87e0cb54b517f75fac0491e
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-07-03 20:33:59 +0530

    Add function to save connection

--- src/sso/connections/utils.lite.tsx
+++ src/sso/connections/utils.lite.tsx
@@ -1,3 +1,56 @@
+import { FormObj } from './types';
+
+export const saveConnection = async ({
+  formObj,
+  isEditView,
+  connectionIsSAML,
+  connectionIsOIDC,
+  setupLinkToken,
+  callback,
+}: {
+  formObj: FormObj;
+  isEditView?: boolean;
+  connectionIsSAML: boolean;
+  connectionIsOIDC: boolean;
+  setupLinkToken?: string;
+  callback: (res: Response) => Promise<void>;
+}) => {
+  const {
+    rawMetadata,
+    redirectUrl,
+    oidcDiscoveryUrl,
+    oidcMetadata,
+    oidcClientId,
+    oidcClientSecret,
+    metadataUrl,
+    ...rest
+  } = formObj;
+
+  const encodedRawMetadata = btoa((rawMetadata as string) || '');
+  const redirectUrlList = (redirectUrl as string)?.split(/\r\n|\r|\n/);
+
+  const res = await fetch(
+    setupLinkToken ? `/api/setup/${setupLinkToken}/sso-connection` : '/api/admin/connections',
+    {
+      method: isEditView ? 'PATCH' : 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        ...rest,
+        encodedRawMetadata: connectionIsSAML ? encodedRawMetadata : undefined,
+        oidcDiscoveryUrl: connectionIsOIDC ? oidcDiscoveryUrl : undefined,
+        oidcMetadata: connectionIsOIDC ? oidcMetadata : undefined,
+        oidcClientId: connectionIsOIDC ? oidcClientId : undefined,
+        oidcClientSecret: connectionIsOIDC ? oidcClientSecret : undefined,
+        redirectUrl: redirectUrl && redirectUrlList ? JSON.stringify(redirectUrlList) : undefined,
+        metadataUrl: connectionIsSAML ? metadataUrl : undefined,
+      }),
+    }
+  );
+  callback(res);
+};
+
 export default function FieldList() {
   return (
     <div>
