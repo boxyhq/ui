2605afac306b617b28f19d038f228678e1798a76
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-07-04 13:44:41 +0530

    Add types for OIDCSSORecord and SAMLSSORecord

--- src/sso/connections/types.ts
+++ src/sso/connections/types.ts
@@ -27,3 +27,82 @@ export type ApiResponse<T = any> = ApiSuccess<T> | { error: ApiError };
 type FormObjValues = string | boolean | string[];
 
 export type FormObj = Record<string, FormObjValues | Record<string, FormObjValues>>;
+
+export interface MtlsEndpointAliases {
+  token_endpoint?: string;
+  userinfo_endpoint?: string;
+  revocation_endpoint?: string;
+  introspection_endpoint?: string;
+  device_authorization_endpoint?: string;
+}
+
+export interface IssuerMetadata {
+  issuer: string;
+  authorization_endpoint?: string;
+  token_endpoint?: string;
+  jwks_uri?: string;
+  userinfo_endpoint?: string;
+  revocation_endpoint?: string;
+  end_session_endpoint?: string;
+  registration_endpoint?: string;
+  token_endpoint_auth_methods_supported?: string[];
+  token_endpoint_auth_signing_alg_values_supported?: string[];
+  introspection_endpoint_auth_methods_supported?: string[];
+  introspection_endpoint_auth_signing_alg_values_supported?: string[];
+  revocation_endpoint_auth_methods_supported?: string[];
+  revocation_endpoint_auth_signing_alg_values_supported?: string[];
+  request_object_signing_alg_values_supported?: string[];
+  mtls_endpoint_aliases?: MtlsEndpointAliases;
+
+  [key: string]: unknown;
+}
+
+interface SSOConnection {
+  defaultRedirectUrl: string;
+  redirectUrl: string[] | string;
+  tenant: string;
+  product: string;
+  name?: string;
+  description?: string;
+}
+
+export interface SAMLSSOConnection extends SSOConnection {
+  forceAuthn?: boolean | string;
+  identifierFormat?: string;
+}
+
+export interface SAMLSSORecord extends SAMLSSOConnection {
+  clientID: string; // set by Jackson
+  clientSecret: string; // set by Jackson
+  metadataUrl?: string;
+  idpMetadata: {
+    entityID: string;
+    loginType?: string;
+    provider: string | 'Unknown';
+    friendlyProviderName: string | null;
+    slo: {
+      postUrl?: string;
+      redirectUrl?: string;
+    };
+    sso: {
+      postUrl?: string;
+      redirectUrl?: string;
+    };
+    thumbprint?: string;
+    validTo?: string;
+  };
+  deactivated?: boolean;
+}
+
+export interface OIDCSSORecord extends SSOConnection {
+  clientID: string; // set by Jackson
+  clientSecret: string; // set by Jackson
+  oidcProvider: {
+    provider?: string;
+    discoveryUrl?: string;
+    metadata?: IssuerMetadata;
+    clientId?: string;
+    clientSecret?: string;
+  };
+  deactivated?: boolean;
+}
