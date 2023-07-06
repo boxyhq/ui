ab354b8d9c0fc1c294d7d352731d6f023f799313
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-07-06 11:36:33 +0530

    Use props instead of destructuring

--- src/shared/IconButton.lite.tsx
+++ src/shared/IconButton.lite.tsx
@@ -1,21 +1,12 @@
 import { classNames } from '../sso/connections/types';
+import { IconButtonProps } from './types';
 
-export default function IconButton({
-  Icon,
-  tooltip,
-  onClick,
-  className,
-}: {
-  Icon: any;
-  tooltip: string;
-  onClick: () => void;
-  className: string;
-}) {
+export default function IconButton(props: IconButtonProps) {
   return (
-    <div className='tooltip' data-tip={tooltip}>
-      <Icon
-        className={classNames('hover:scale-115 h-5 w-5 cursor-pointer text-secondary', className)}
-        onClick={onClick}
+    <div className='tooltip' data-tip={props.tooltip}>
+      <props.Icon
+        className={classNames('hover:scale-115 h-5 w-5 cursor-pointer text-secondary', props.className)}
+        onClick={() => props.onClick()}
       />
     </div>
   );
