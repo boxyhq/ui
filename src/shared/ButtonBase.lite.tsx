fd7f2026f971aa258c92464eff67390745e06fac
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-07-06 17:28:48 +0530

    Using a getter to calculate value of computed property classNames

--- src/shared/ButtonBase.lite.tsx
+++ src/shared/ButtonBase.lite.tsx
@@ -1,3 +1,5 @@
+import { useStore } from '@builder.io/mitosis';
+
 export interface ButtonBaseProps {
   Icon?: any;
   loading?: any;
@@ -7,8 +9,14 @@ export interface ButtonBaseProps {
 }
 
 export default function ButtonBase({ Icon, children, color, buttonName }: ButtonBaseProps) {
+  const state = useStore({
+    get classNames() {
+      return `btn h-4 w-4 ${children ? 'mr-1' : ''} ${color}`;
+    },
+  });
+
   return (
-    <button className={`btn h-4 w-4 ${children ? 'mr-1' : ''} ${color}`}>
+    <button className={state.classNames}>
       {Icon}
       {buttonName}
       {children}
