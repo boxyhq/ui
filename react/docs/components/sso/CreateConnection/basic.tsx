3d39a65df565931c878f8d0b0b82c31d79bd12f5
Aswin V <vaswin91@gmail.com>
2023-07-08 17:19:09 +0530

    Refactor react/docs

---
+++ react/docs/components/sso/CreateConnection/basic.tsx
@@ -0,0 +1,24 @@
+/**
+ * @title CreateSAMLConnection basic variant
+ * @description Refer the code below to see the passed props. Also supported is the passing of style attribute for each inner element (Note that inline style will override other styles).
+ * @order 1
+ */
+import { CreateSAMLConnection } from '@boxyhq/react-ui/sso';
+import '../CustomStyling.css';
+
+export default () => {
+  return (
+    <CreateSAMLConnection
+      loading={false}
+      cb={undefined}
+      t={(key) => key}
+      connectionIsSAML={false}
+      connectionIsOIDC={false}
+      errorToastCallback={function (errMessage: string): void {
+        throw new Error('Function not implemented.');
+      }}
+      variant={'basic'}
+      classNames={{ input: 'inp' }}
+    />
+  );
+};
