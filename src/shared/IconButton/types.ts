75c00b00a120190b8aa1b517f3b6d2c691e42894
Aswin V <vaswin91@gmail.com>
2023-07-09 22:39:42 +0530

    Type refactor

--- src/shared/IconButton/types.ts
+++ src/shared/IconButton/types.ts
@@ -3,6 +3,6 @@ import type { JSX } from '@builder.io/mitosis/jsx-runtime';
 export interface IconButtonProps {
   Icon: (props: JSX.SvgSVGAttributes<SVGSVGElement>) => JSX.Element;
   label: string;
-  onClick: () => void;
+  onClick: (event: any) => void;
   iconClasses: string;
 }
