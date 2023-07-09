05ff8f79f50defd57aa7fd23e3c18e372197ccb7
Aswin V <vaswin91@gmail.com>
2023-07-09 22:41:04 +0530

    Fixes - export of mitosis component, - move method inside `useStore`

--- src/shared/ClipboardButton/index.lite.tsx
+++ src/shared/ClipboardButton/index.lite.tsx
@@ -1,72 +1,32 @@
 import { useStore } from '@builder.io/mitosis';
 import IconButton from '../IconButton/index.lite';
-import baseStyles from './common.module.css';
-import styles from './index.module.css';
 import CopytoClipboardIcon from '../icons/CopytoClipboardIcon.lite';
 
-export function copyToClipboard(text: string) {
-  navigator.clipboard.writeText(text);
+interface PropsType {
+  text: string;
+  translation: any;
+  toastSuccessCallback: () => void;
 }
 
-export const CopyToClipboardButton = ({
-  text,
-  translation,
-  toastSuccessCallback,
-}: {
-  text: string;
-  translation: any;
-  toastSuccessCallback: () => void;
-}) => {
-  return (
-    <IconButton
-      label={translation('copy')}
-      Icon={CopytoClipboardIcon}
-      iconClasses='hover:text-primary'
-      onClick={() => {
-        copyToClipboard(text);
-        toastSuccessCallback();
-      }}></IconButton>
-  );
-};
-
-export function InputWithCopyButton({
-  text,
-  label,
-  translation,
-  toastSuccessCallback,
-  classNames,
-}: {
-  text: string;
-  label: string;
-  translation: any;
-  toastSuccessCallback: () => void;
-  classNames: {
-    label: string;
-    input: string;
-  };
-}) {
+export default function CopyToClipboardButton(props: PropsType) {
   const state = useStore({
-    id: label.replace(/ /g, ''),
-    get classes() {
-      return {
-        label: styles.label + (classNames?.label ? ` ${classNames.label}` : ''),
-        input: baseStyles.input + (classNames?.input ? ` ${classNames.input}` : ''),
-      };
+    get label() {
+      return props.translation('copy');
+    },
+    copyToClipboard(text: string) {
+      navigator.clipboard.writeText(text);
+    },
+    onClick: () => {
+      state.copyToClipboard(props.text);
+      props.toastSuccessCallback();
     },
   });
   return (
-    <>
-      <div class={`${baseStyles.flex} ${baseStyles['justify-between']}`}>
-        <label class={state.classes.label} for={state.id}>
-          {label}
-        </label>
-        <CopyToClipboardButton
-          toastSuccessCallback={toastSuccessCallback}
-          translation={translation}
-          text={text}
-        />
-      </div>
-      <input id={state.id} type='text' value={text} key={text} readOnly class={state.classes.input} />
-    </>
+    <IconButton
+      label={state.label}
+      Icon={CopytoClipboardIcon}
+      iconClasses='hover:text-primary'
+      onClick={state.onClick}
+    />
   );
 }
