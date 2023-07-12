aa661c2497b440c7825ddd3cb7c97d5e4ab98ce6
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-07-12 11:13:10 +0530

    Include ConnectionList in exports

--- src/sso/index.ts
+++ src/sso/index.ts
@@ -2,3 +2,4 @@ export { default as Login } from './login/index.lite';
 export { default as CreateConnection } from './connections/CreateConnection/index.lite';
 export { default as CreateSAMLConnection } from './connections/CreateConnection/saml/index.lite';
 export { default as CreateOIDCConnection } from './connections/CreateConnection/oidc/index.lite';
+export { default as ConnectionList } from './connections/ConnectionList/index.lite';
