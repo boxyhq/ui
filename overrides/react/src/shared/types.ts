d1b37a12835a97d0cba3e0eb3d06d06aeb602b13
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-07-13 22:05:10 +0530

    Add props interfaces  and update overrides

--- overrides/react/src/shared/types.ts
+++ overrides/react/src/shared/types.ts
@@ -25,3 +25,48 @@ export interface BadgeProps {
   size?: string;
   ariaLabel?: string;
 }
+
+export interface ModalProps {
+  visible: boolean;
+  title: string;
+  description?: string;
+  children?: any;
+}
+
+export interface ConfirmationModalProps {
+  visible: boolean;
+  title: string;
+  description: string;
+  onConfirm: () => void | Promise<void>;
+  onCancel: () => void;
+  actionButtonText?: string;
+  overrideDeleteButton?: boolean;
+  dataTestId?: string;
+  translation?: any;
+}
+
+export interface ButtonBaseProps {
+  children?: any;
+  color?: string;
+  iconClasses?: string;
+  label?: string;
+  onClick: (event: any) => void;
+}
+
+export interface ButtonProps {
+  children?: string;
+  label?: string;
+  color?: string;
+  onClick: () => void;
+  dataTestId?: string;
+  classNames?: string;
+}
+
+export interface ConnectionToggleProps {
+  onChange: (active: boolean) => void;
+  connection: {
+    active: boolean;
+    type: 'sso' | 'dsync';
+  };
+  translation?: any;
+}
