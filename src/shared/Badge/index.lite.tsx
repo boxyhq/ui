d498884e3af8b767c63c9d5ab77161ba243bc575
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-07-13 21:08:44 +0530

    Replace divs with span

--- src/shared/Badge/index.lite.tsx
+++ src/shared/Badge/index.lite.tsx
@@ -2,11 +2,11 @@ import { BadgeProps } from '../types';
 
 export default function Badge(props: BadgeProps) {
   return (
-    <div
+    <span
       color={props.color}
       aria-label={props.ariaLabel}
       class={`rounded-md py-2 text-white ${props.className}`}>
       {props.children}
-    </div>
+    </span>
   );
 }
