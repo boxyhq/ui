e8189d35a91cdb9a8877f5cca01a07084eb7b0f6
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-07-14 12:17:31 +0530

    Update ConnectionToggle view

--- src/shared/ConnectionToggle/index.lite.tsx
+++ src/shared/ConnectionToggle/index.lite.tsx
@@ -1,7 +1,63 @@
-export default function ConnectionToggle() {
+import { useStore } from '@builder.io/mitosis';
+import { ConnectionToggleProps } from '../types';
+import ConfirmationModal from '../ConfirmationModal/index.lite';
+
+export default function ConnectionToggle(props: ConnectionToggleProps) {
+  const state = useStore({
+    isModalVisible: false,
+    active: props.connection.active,
+    confirmationModalDescription: {
+      sso: {
+        activate: props.translation('activate_sso_connection_description'),
+        deactivate: props.translation('deactivate_sso_connection_description'),
+      },
+      dsync: {
+        activate: props.translation('activate_dsync_connection_description'),
+        deactivate: props.translation('deactivate_dsync_connection_description'),
+      },
+    }[props.connection.type][props.connection.active ? 'deactivate' : 'activate'],
+    get confirmationModalTitle() {
+      return this.active
+        ? props.translation('deactivate_connection')
+        : props.translation('activate_connection');
+    },
+    get ConnectinStatusMessage() {
+      return this.active ? props.translation('active') : props.translation('inactive');
+    },
+    askForConfirmation() {
+      state.isModalVisible = true;
+    },
+    onCancel() {
+      state.isModalVisible = false;
+    },
+    onConfirm() {
+      state.isModalVisible = false;
+      state.active = !state.active;
+      props.onChange(!state.active);
+    },
+  });
+
   return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
+    <>
+      <label className='label cursor-pointer'>
+        <span className='label-text mr-2'>{state.ConnectinStatusMessage}</span>
+        <input
+          type='checkbox'
+          className='toggle-success toggle'
+          onChange={(event) => state.askForConfirmation()}
+          checked={state.active}
+        />
+      </label>
+      <ConfirmationModal
+        title={state.confirmationModalTitle}
+        description={state.confirmationModalDescription}
+        actionButtonText={props.translation('yes_proceed')}
+        overrideDeleteButton={true}
+        visible={state.isModalVisible}
+        onConfirm={state.onConfirm}
+        onCancel={state.onCancel}
+        dataTestId='confirm-connection-toggle'
+      />
+    </>
   );
 }
