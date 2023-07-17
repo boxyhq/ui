948a4982372fa8d1dd16a5f31eecb2a5ade137ed
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-07-17 12:41:11 +0530

    Tweak badge icon styles

--- src/shared/Badge/index.lite.tsx
+++ src/shared/Badge/index.lite.tsx
@@ -2,10 +2,7 @@ import { BadgeProps } from '../types';
 
 export default function Badge(props: BadgeProps) {
   return (
-    <span
-      color={props.color}
-      aria-label={props.ariaLabel}
-      class={`rounded-md py-2 text-white ${props.className}`}>
+    <span color={props.color} aria-label={props.ariaLabel} class={`rounded-md py-2 ${props.className}`}>
       {props.children}
     </span>
   );
