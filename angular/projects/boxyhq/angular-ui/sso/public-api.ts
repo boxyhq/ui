625dac2e7d320fca27a2ce8a067f51e6a4d6cc41
Aswin V <vaswin91@gmail.com>
2023-07-24 11:46:05 +0530

    [Angular] Add new components inside public api and main index

--- angular/projects/boxyhq/angular-ui/sso/public-api.ts
+++ angular/projects/boxyhq/angular-ui/sso/public-api.ts
@@ -1,1 +1,5 @@
 export * from './src/login';
+export * from './src/connections/CreateConnection';
+export * from './src/connections/CreateConnection/saml';
+export * from './src/connections/CreateConnection/oidc';
+export * from './src/connections/ConnectionList';
