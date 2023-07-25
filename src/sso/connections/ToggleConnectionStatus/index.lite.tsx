5bb3c9cc49f2f19fe479245ac2ac8e1c2d5f4b4c
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-07-25 11:38:55 +0530

    Update toggle button with missing state values and remove modal

--- src/sso/connections/ToggleConnectionStatus/index.lite.tsx
+++ src/sso/connections/ToggleConnectionStatus/index.lite.tsx
@@ -1,15 +1,83 @@
-import { useStore, onMount, onUpdate } from '@builder.io/mitosis';
+import { useStore, onMount, onUpdate, Show } from '@builder.io/mitosis';
 import type { ToggleConnectionStatusProps } from '../types';
+import { ApiResponse } from '../types';
+import defaultClasses from './index.module.css';
+import cssClassAssembler from '../../utils/cssClassAssembler';
 
 export default function ToggleConnectionStatus(props: ToggleConnectionStatusProps) {
   const state = useStore({
-    active: !props.connection.deactivated,
-    isModalVisible: false,
-    get ConnectinStatusMessage() {
-      return !props.connection.deactivated ? props.translation('active') : props.translation('inactive');
+    active: false,
+    displayConnectionMessage: true,
+    get ConnectionStatusMessage() {
+      return this.active ? 'Active' : 'Inactive';
+    },
+    get connectActivate() {
+      return this.active ? 'deactivate' : 'activate';
     },
     askForConfirmation() {
-      state.isModalVisible = true;
+      state.displayConnectionMessage = false;
+    },
+    onCancel() {
+      state.displayConnectionMessage = true;
+    },
+    onConfirm() {
+      state.active = !state.active;
+      state.updateConnectionStatus(!state.active);
+      state.displayConnectionMessage = true;
+    },
+    get classes() {
+      return {
+        container: cssClassAssembler(props.classNames?.container, defaultClasses.container),
+        heading: cssClassAssembler(props.classNames?.heading, defaultClasses.heading),
+        alohaa: cssClassAssembler(props.classNames?.alohaa, defaultClasses.alohaa),
+        confirmBtn: cssClassAssembler(props.classNames?.confirmBtn, defaultClasses.confirmBtn),
+        cancelBtn: cssClassAssembler(props.classNames?.cancelBtn, defaultClasses.cancelBtn),
+        toggle: cssClassAssembler(props.classNames?.toggle, defaultClasses.toggle),
+        displayMessage: cssClassAssembler(props.classNames?.displayMessage, defaultClasses.displayMessage),
+        toggleTransition: cssClassAssembler(
+          props.classNames?.toggleTransition,
+          defaultClasses.toggleTransition
+        ),
+      };
+    },
+    updateConnectionStatus(isConnectionActive: boolean) {
+      void (async function () {
+        state.active = isConnectionActive;
+
+        const body = {
+          clientID: props.connection?.clientID,
+          clientSecret: props.connection?.clientSecret,
+          tenant: props.connection?.tenant,
+          product: props.connection?.product,
+          deactivated: !state.active,
+        };
+
+        if ('idpMetadata' in props.connection) {
+          body['isSAML'] = true;
+        } else {
+          body['isOIDC'] = true;
+        }
+
+        const res = await fetch(props.urls.save, {
+          method: 'PATCH',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(body),
+        });
+        const response: ApiResponse = await res.json();
+
+        if ('error' in response) {
+          props.errorCallback(response.error.message);
+          return;
+        }
+
+        if (body.deactivated) {
+          props.successCallback('Connection Deactivated');
+        } else {
+          props.successCallback('Connection Activated');
+        }
+      })();
     },
   });
 
@@ -22,16 +90,32 @@ export default function ToggleConnectionStatus(props: ToggleConnectionStatusProp
   }, [props.connection]);
 
   return (
-    <>
-      <label className='label cursor-pointer'>
-        <span className='label-text mr-2'>{state.ConnectinStatusMessage}</span>
+    <div class={state.classes.container}>
+      <Show when={!state.displayConnectionMessage}>
+        <div class={state.classes.displayMessage}>
+          <h1>
+            Do you want to<span>{` ${state.connectActivate} `}</span>connection status?
+          </h1>
+          <button class={state.classes.confirmBtn} onClick={(event) => state.onConfirm()}>
+            Confirm
+          </button>
+          <button class={state.classes.cancelBtn} onClick={(event) => state.onCancel()}>
+            Cancel
+          </button>
+        </div>
+      </Show>
+      <Show when={state.displayConnectionMessage}>
+        <label class='label'>
+          <span class='label-text mr-2'>{state.ConnectionStatusMessage}</span>
+        </label>
         <input
           type='checkbox'
-          className='toggle-success toggle'
+          id='toggle-status'
+          class={`toggle-success toggle`}
           onChange={(event) => state.askForConfirmation()}
           checked={state.active}
         />
-      </label>
-    </>
+      </Show>
+    </div>
   );
 }
