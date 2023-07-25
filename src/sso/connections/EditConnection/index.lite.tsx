9ef2b17208a8ae740ff78b997b3f89a4500acf62
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-07-25 14:05:22 +0530

    Update view for EditConnection component

--- src/sso/connections/EditConnection/index.lite.tsx
+++ src/sso/connections/EditConnection/index.lite.tsx
@@ -1,7 +1,28 @@
-export default function EditConnection() {
+import { useStore } from '@builder.io/mitosis';
+import type { EditConnectionProps } from '../types';
+import ToggleConnectionStatus from '../ToggleConnectionStatus/index.lite';
+
+export default function EditConnection(props: EditConnectionProps) {
+  const state = useStore({
+    get connectionIsSAML() {
+      return 'idpMetadata' in props.connection && typeof props.connection.idpMetadata === 'object';
+    },
+    get connectionIsOIDC() {
+      return 'oidcProvider' in props.connection && typeof props.connection.oidcProvider === 'object';
+    },
+  });
+
   return (
     <div>
-      <h1>This is the parent component for edit connection!</h1>
+      <div className='flex items-center justify-between'>
+        <h2 className='mb-5 mt-5 font-bold text-gray-700 dark:text-white md:text-xl'>Edit SSO Connection</h2>
+        <ToggleConnectionStatus
+          connection={props.connection}
+          urls={props.toggleConnectionUrls}
+          errorCallback={props.errorCallback}
+          successCallback={props.successCallback}
+        />
+      </div>
     </div>
   );
 }
