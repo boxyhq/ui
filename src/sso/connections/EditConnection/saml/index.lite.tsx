0a3610b4a30d403d0db4598ebf5b4d85a2188158
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-07-26 12:06:32 +0530

    Add editable fields and update button classes

--- src/sso/connections/EditConnection/saml/index.lite.tsx
+++ src/sso/connections/EditConnection/saml/index.lite.tsx
@@ -7,7 +7,124 @@ export default function EditSAMLConnection(props: EditSAMLConnectionProps) {
       <div class='min-w-[28rem] rounded border border-gray-200 bg-white p-6 dark:border-gray-700 dark:bg-gray-800 lg:border-none lg:p-0'>
         <div class='flex flex-col gap-0 lg:flex-row lg:gap-4'>
           <div class='w-full rounded border-gray-200 dark:border-gray-700 lg:w-3/5 lg:border lg:p-3'>
-            <h1>Editable fields</h1>
+            <div class='mb-6'>
+              <div class='flex items-center justify-between'>
+                <label for='name' class='mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300'>
+                  Name
+                </label>
+              </div>
+              <input
+                class='input-bordered input w-full'
+                type='text'
+                name='name'
+                id='name'
+                placeholder='MyApp'
+                value={props.connection.name}
+              />
+            </div>
+            <div class='mb-6'>
+              <div class='flex items-center justify-between'>
+                <label
+                  for='description'
+                  class='mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300'>
+                  Description
+                </label>
+              </div>
+              <input
+                class='input-bordered input w-full'
+                type='text'
+                name='description'
+                id='description'
+                placeholder='A short description not more than 100 characters'
+                maxLength={100}
+                value={props.connection.description}
+              />
+            </div>
+            <div class='mb-6'>
+              <div class='flex items-center justify-between'>
+                <label
+                  for='redirectUrl'
+                  class='mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300'>
+                  Allowed redirect URLs (newline separated)
+                </label>
+              </div>
+              <textarea
+                class='textarea-bordered textarea h-24 w-full whitespace-pre'
+                id='redirectUrl'
+                name='redirectUrl'
+                required={true}
+                rows={3}
+                placeholder='http://localhost:3366'
+                value={props.connection.redirectUrl}
+              />
+            </div>
+            <div class='mb-6'>
+              <div class='flex items-center justify-between'>
+                <label
+                  for='defaultRedirectUrl'
+                  class='mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300'>
+                  Default redirect URL
+                </label>
+              </div>
+              <input
+                class='input-bordered input w-full'
+                name='defaultRedirectUrl'
+                id='defaultRedirectUrl'
+                required={true}
+                type='url'
+                placeholder='http://localhost:3366/login/saml'
+                value={props.connection.defaultRedirectUrl}
+              />
+            </div>
+            <div class='mb-6'>
+              <div class='flex items-center justify-between'>
+                <label
+                  for='rawMetadata'
+                  class='mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300'>
+                  Raw IdP XML (fully replaces the current one)
+                </label>
+              </div>
+              <textarea
+                class='textarea-bordered textarea h-24 w-full'
+                name='rawMetadata'
+                id='rawMetadata'
+                placeholder='Paste the raw XML here'
+                rows={5}
+                value={props.connection.rawMetadata}
+              />
+            </div>
+            <div class='mb-6'>
+              <div class='flex items-center justify-between'>
+                <label
+                  for='metadataUrl'
+                  class='mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300'>
+                  Metadata URL (fully replaces the current one)
+                </label>
+              </div>
+              <input
+                class='input-bordered input w-full'
+                name='metadataUrl'
+                id='metadataUrl'
+                type='url'
+                placeholder='Paste the Metadata URL here'
+                value={props.connection.metadataUrl}
+              />
+            </div>
+            <div class='mb-6'>
+              <div class='flex items-center justify-between'>
+                <label
+                  for='forceAuthn'
+                  class='mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300'>
+                  Force Authentication
+                </label>
+              </div>
+              <input
+                class='checkbox-primary checkbox ml-5 align-middle'
+                name='forceAuthn'
+                id='forceAuthn'
+                type='checkbox'
+              />
+            </div>
           </div>
           <div class='w-full rounded border-gray-200 dark:border-gray-700 lg:w-3/5 lg:border lg:p-3'>
             <div class='mb-6'>
@@ -108,8 +225,9 @@ export default function EditSAMLConnection(props: EditSAMLConnectionProps) {
             </div>
           </div>
           <div className='flex w-full lg:mt-6'>
-            {/* ButtonPrimary goes here */}
-            <button type='submit'>Save Changes</button>
+            <button type='submit' class='btn btn-primary'>
+              Save Changes
+            </button>
           </div>
         </div>
       </div>
@@ -119,8 +237,9 @@ export default function EditSAMLConnection(props: EditSAMLConnectionProps) {
             <h6 class='mb-1 font-medium'>Delete this connection</h6>
             <p class='font-light'>All your apps using this connection will stop working.</p>
           </div>
-          {/* Danger button goes here */}
-          <button type='button'>Delete</button>
+          <button type='button' class='btn btn-error'>
+            Delete
+          </button>
         </section>
       </Show>
     </form>
