ded2fb7996a50e464aef5bff1d967547d154c098
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-08-01 12:01:53 +0530

    Include edit connection parent component

--- src/sso/index.ts
+++ src/sso/index.ts
@@ -3,5 +3,6 @@ export { default as CreateSSOConnection } from './connections/CreateConnection/i
 export { default as CreateSAMLConnection } from './connections/CreateConnection/saml/index.lite';
 export { default as CreateOIDCConnection } from './connections/CreateConnection/oidc/index.lite';
 export { default as ConnectionList } from './connections/ConnectionList/index.lite';
+export { default as EditConnection } from './connections/EditConnection/index.lite';
 export { default as EditOIDCConnection } from './connections/EditConnection/oidc/index.lite';
 export { default as EditSAMLConnection } from './connections/EditConnection/saml/index.lite';
