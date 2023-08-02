0d82db919c0a0df8b891d733e8e2bb7cf25362f2
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-08-02 19:02:55 +0530

    Create secret input control for password type

---
+++ src/shared/SecretInputFormControl/index.lite.tsx
@@ -0,0 +1,46 @@
+import { useStore } from '@builder.io/mitosis';
+import { SecretInputFormControlProps } from '../types';
+import CopyToClipboardButton from '../ClipboardButton/index.lite';
+import IconButton from '../IconButton/index.lite';
+import EyeIcon from '../icons/EyeIcon.lite';
+import EyeSlashIcon from '../icons/EyeSlashIcon.lite';
+
+export default function SecretInputFormControl(props: SecretInputFormControlProps) {
+  const state = useStore({
+    isSecretShown: false,
+  });
+
+  return (
+    <div class='mb-6'>
+      <div class='flex items-center justify-between'>
+        <label
+          htmlFor={props.id}
+          class={`mb-2 block text-sm font-medium text-gray-900 dark:text-gray-300 ${props.isHiddenClassName}`}>
+          {props.label}
+        </label>
+        <div class='flex'>
+          <IconButton
+            Icon={state.isSecretShown ? EyeSlashIcon : EyeIcon}
+            iconClasses='hover:text-primary mr-2'
+            onClick={() => {
+              state.isSecretShown = !state.isSecretShown;
+            }}
+          />
+          <CopyToClipboardButton text={props.value} toastSuccessCallback={props.successCallback} />
+        </div>
+      </div>
+      <input
+        type={state.isSecretShown ? 'text' : 'password'}
+        placeholder={props.placeholder}
+        value={props.value || ''}
+        id={props.id}
+        required={props.required}
+        maxLength={props.maxLength}
+        readOnly={props.readOnly}
+        class={`input-bordered input w-full ${props.isHiddenClassName} ${
+          props.readOnly ? ' bg-gray-50' : ''
+        }`}
+      />
+    </div>
+  );
+}
