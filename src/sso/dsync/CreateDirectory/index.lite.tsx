882f954b0cddacd65e1f1a16ccc561070889d316
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-08-04 13:57:20 +0530

    Init directory sync folder

---
+++ src/sso/dsync/CreateDirectory/index.lite.tsx
@@ -0,0 +1,7 @@
+export default function CreateDirectory() {
+  return (
+    <div>
+      <h1>This is the component to create a directory!</h1>
+    </div>
+  );
+}
---
+++ src/sso/dsync/DeleteDirectory/index.lite.tsx
@@ -0,0 +1,7 @@
+export default function DeleteDirectory() {
+  return (
+    <div>
+      <h1>This is the component to delete a directory!</h1>
+    </div>
+  );
+}
