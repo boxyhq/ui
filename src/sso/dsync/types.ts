e4a9f71032a4287f591b4ae712da4f03b10402f2
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-08-07 20:52:19 +0530

    Typings for dsync

---
+++ src/sso/dsync/types.ts
@@ -0,0 +1,53 @@
+export interface CreateDirectoryProps {
+  urls: {
+    patchUrl: string;
+    useDirectoryProviderUrl: string;
+  };
+  defaultWebhookEndpoint?: string | undefined;
+  setupLinkToken?: string;
+  cb: () => void;
+  successCallback: (successMsg: string) => void;
+  errorCallback: (errMsg: string) => void;
+}
+
+export type ApiSuccess<T> = { data: T; pageToken?: string };
+
+export interface ApiError extends Error {
+  info?: string;
+  status: number;
+}
+
+export type ApiResponse<T = any> = ApiSuccess<T> | { error: ApiError };
+
+export enum DirectorySyncProviders {
+  'azure-scim-v2' = 'Azure SCIM v2.0',
+  'onelogin-scim-v2' = 'OneLogin SCIM v2.0',
+  'okta-scim-v2' = 'Okta SCIM v2.0',
+  'jumpcloud-scim-v2' = 'JumpCloud v2.0',
+  'generic-scim-v2' = 'Generic SCIM v2.0',
+  'google' = 'Google',
+}
+
+export type DirectoryType = keyof typeof DirectorySyncProviders;
+
+export type Directory = {
+  id: string;
+  name: string;
+  tenant: string;
+  product: string;
+  type: DirectoryType;
+  log_webhook_events: boolean;
+  scim: {
+    path: string;
+    endpoint?: string;
+    secret: string;
+  };
+  webhook: {
+    endpoint: string;
+    secret: string;
+  };
+  deactivated?: boolean;
+  google_domain?: string;
+  google_access_token?: string;
+  google_refresh_token?: string;
+};
