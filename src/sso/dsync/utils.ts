0f15b9fe874f1745719b341c6701b760f3d737be
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-08-08 12:55:11 +0530

    Remove unused utils file

--- src/sso/dsync/utils.ts
+++
@@ -1,9 +0,0 @@
-export async function useDirectoryProviders(url: string) {
-  const response = await fetch(url);
-  const { data, error } = await response.json();
-
-  return {
-    providers: data?.data,
-    error,
-  };
-}
