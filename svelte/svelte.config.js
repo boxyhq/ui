17a7fb98cdb77beb73c6ae2a3aa45e1bbe649f8e
Aswin V <vaswin91@gmail.com>
2023-08-14 01:27:11 +0530

    Add shared folder path to alias

--- svelte/svelte.config.js
+++ svelte/svelte.config.js
@@ -13,7 +13,8 @@ const config = {
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 		alias: {
-			'@boxyhq/svelte-ui/sso': 'dist/sso/index.js'
+			'@boxyhq/svelte-ui/sso': 'dist/sso/index.js',
+			'@/shared/*': 'dist/shared/*'
 		}
 	}
 };
