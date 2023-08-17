9bdde9ee6fca3794f591cad8ad53f831b905e678
Aswin V <vaswin91@gmail.com>
2023-08-17 14:14:39 +0530

    Remove internal components

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,3 +1,1 @@
-export { default as CopyToClipboardButton } from './CopyToClipboardButton/index.lite';
-export { default as InputWithCopyButton } from './InputWithCopyButton/index.lite';
 export { default as Themer } from './Themer/index.lite';
