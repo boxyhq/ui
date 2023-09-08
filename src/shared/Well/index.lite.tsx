65a092cefe506e5c85cd7b550b85ef39a89b027b
Aswin V <vaswin91@gmail.com>
2023-09-08 21:39:37 +0530

    Fix `Well` children prop

--- src/shared/Well/index.lite.tsx
+++ src/shared/Well/index.lite.tsx
@@ -1,7 +1,6 @@
-import { JSX } from "@builder.io/mitosis/jsx-runtime";
 import styles from "./index.module.css";
 
 // Adds a rounded border around some content
-export default function Well(props: { children: JSX.Element }) {
+export default function Well(props: { children?: any }) {
     return <div class={styles.well}>{props.children}</div>
 }
