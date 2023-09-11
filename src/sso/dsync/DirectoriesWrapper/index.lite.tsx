196be79ccb59365a496d79ccb4a74c573eafa581
Ryukemeister <sahalrajiv-extc@atharvacoe.ac.in>
2023-09-11 12:13:52 +0530

    Update directories wrapper view

--- src/sso/dsync/DirectoriesWrapper/index.lite.tsx
+++ src/sso/dsync/DirectoriesWrapper/index.lite.tsx
@@ -1,7 +1,38 @@
+import { useStore, Show } from '@builder.io/mitosis';
+import type { Directory } from '../types';
+
+const DEFAULT_VALUES = {
+  directoryListData: [] as Directory[],
+  view: 'LIST' as 'LIST' | 'EDIT' | 'CREATE',
+};
+
 export default function DirectoriesWrapper() {
+  const state = useStore({
+    directories: DEFAULT_VALUES.directoryListData,
+    view: DEFAULT_VALUES.view,
+    directoryToEdit: {} as Directory,
+    switchToCreateView() {
+      state.view = 'CREATE';
+    },
+    switchToEditView(action: 'edit', directory: any) {
+      state.view = 'EDIT';
+      state.directoryToEdit = directory;
+    },
+    switchToListView() {
+      state.view = 'LIST';
+    },
+    logError(err: string) {
+      console.error(err);
+    },
+  });
+
   return (
     <div>
-      <h1>This is the directories wrapper compnonent!</h1>
+      <div class='flex flex-col'>
+        <Show when={state.view === 'LIST'}></Show>
+      </div>
+      <Show when={state.view === 'EDIT'}></Show>
+      <Show when={state.view === 'CREATE'}></Show>
     </div>
   );
 }
