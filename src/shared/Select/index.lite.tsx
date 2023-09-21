f6116efe4ae490983acaf9291e1046599a2c64d7
Aswin V <vaswin91@gmail.com>
2023-09-21 16:39:44 +0800

    Disabled state handling and styling fixes

--- src/shared/Select/index.lite.tsx
+++ src/shared/Select/index.lite.tsx
@@ -1,20 +1,26 @@
 import { For, Fragment, useStore } from "@builder.io/mitosis";
 import styles from "./index.module.css"
+import Spacer from "../Spacer/index.lite";
 
 interface SelectProps {
     label: string;
-    options: Array<{ value: string, text: string }>
+    options: Array<{ value: string, text: string }>;
+    disabled?: boolean;
 }
 export default function Select(props: SelectProps) {
     const state = useStore({
         id: props.label.replace(/ /g, ''),
+        get divCss() {
+            return styles.div + (props.disabled ? ` ${styles["div--disabled"]}` : '')
+        }
     })
 
     return (
         <Fragment>
-            <label htmlFor={state.id}>{props.label}</label>
-            <div class={styles.div}>
-                <select id={state.id} class={styles.select}>
+            <label htmlFor={state.id} class={styles.label}>{props.label}</label>
+            <Spacer y={2} />
+            <div class={state.divCss}>
+                <select id={state.id} class={styles.select} disabled={props.disabled ?? false}>
                     <For each={props.options}>
                         {(optionItem) => (
                             <option value={optionItem.value}>{optionItem.text}
