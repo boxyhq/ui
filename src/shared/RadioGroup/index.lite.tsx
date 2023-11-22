5ddcc037066626ac13fc77ec54308da0d19cdc79
Aswin V <vaswin91@gmail.com>
2023-11-22 17:24:05 +0800

    Tweaks to radio component styles

--- src/shared/RadioGroup/index.lite.tsx
+++ src/shared/RadioGroup/index.lite.tsx
@@ -1,6 +1,7 @@
-import { useStore } from '@builder.io/mitosis';
+import { Show, useStore } from '@builder.io/mitosis';
 import styles from './index.module.css';
 import { RadioGroupProps } from '../types';
+import Spacer from '../Spacer/index.lite';
 
 export default function RadioGroup(props: RadioGroupProps) {
   const state = useStore({
@@ -20,6 +21,12 @@ export default function RadioGroup(props: RadioGroupProps) {
       <div class={styles.label} id={state.id}>
         {props.label}
       </div>
+      <Show when={state.orientationValue === 'horizontal'}>
+        <Spacer x={1} />
+      </Show>
+      <Show when={state.orientationValue === 'horizontal'}>
+        <Spacer y={1} />
+      </Show>
       <div class={styles.inputs}>{props.children}</div>
     </div>
   );
