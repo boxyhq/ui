88b9c0f42439e49969f98ecd576ba8b665562177
Aswin V <vaswin91@gmail.com>
2023-12-19 17:01:40 +0530

    Improve `fetch` wrapper

--- src/shared/fetchData.ts
+++ src/shared/fetchData.ts
@@ -1,12 +1,31 @@
-export async function fetchData(url: string) {
+async function parseResponseContent(response: Response) {
+  const responseText = await response.text();
+
   try {
-    const response = await fetch(url);
-    const json = await response.json();
+    return responseText.length ? JSON.parse(responseText) : '';
+  } catch (err) {
+    return responseText;
+  }
+}
+
+/** undefined for 204 No content */
+type ApiSuccess<T> = (T & { pageToken?: string }) | undefined;
+
+export type ApiResponse<T = any> = ApiSuccess<T> | { error: { message: string } };
+
+export async function fetchData<T = any>(url: string, options?: RequestInit): Promise<ApiResponse<T>> {
+  try {
+    const response = await fetch(url, options);
+    if (response.status === 204) {
+      return;
+    }
+    const responseContent = await parseResponseContent(response);
 
     if (!response.ok) {
-      throw new ApiError(response.status, json.error.message);
+      throw new ApiError(response.status, responseContent.error.message);
     }
-    return json;
+
+    return responseContent;
   } catch (error: any) {
     const message = error.message || 'Something went wrong';
     return { error: { message } };
