76250433190ad417cd63e3703ad5bf04e63f5af7
Aswin V <vaswin91@gmail.com>
2023-12-22 18:20:09 +0530

    Fix button loading state (spinner) styles

--- src/shared/Spinner/index.lite.tsx
+++ src/shared/Spinner/index.lite.tsx
@@ -1,5 +1,19 @@
+import { useStore } from '@builder.io/mitosis';
 import styles from './index.module.css';
 
-export default function Spinner() {
-  return <div class={styles.spinner} aria-hidden={true} aria-label='Loading'></div>;
+interface SpinnerProps {
+  variant?: 'small';
+  /** default var(--primary-color) */
+  color?: 'currentColor';
+}
+export default function Spinner(props: SpinnerProps) {
+  const state = useStore({
+    get css() {
+      return (
+        (props.variant ? ' ' + styles[props.variant] : '') +
+        (props.color === 'currentColor' ? ' ' + styles[props.color] : '')
+      );
+    },
+  });
+  return <span class={`${styles.spinner}${state.css}`} aria-hidden={true} aria-label='Loading'></span>;
 }
