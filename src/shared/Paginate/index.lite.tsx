6323924c27363bc1e36c4248e51ef56a15ac1f73
Aswin V <vaswin91@gmail.com>
2024-01-18 01:44:14 +0530

    WIP `Paginate`

--- src/shared/Paginate/index.lite.tsx
+++ src/shared/Paginate/index.lite.tsx
@@ -1,8 +1,37 @@
-export default function Paginate() {
+import { useStore } from '@builder.io/mitosis';
+import Button from '../Button/index.lite';
+import { PaginateProps } from '../types';
+
+export default function Paginate(props: PaginateProps) {
+  const state = useStore({
+    itemOffset: 0,
+    get isPreviousDisabled() {
+      return state.itemOffset === 0;
+    },
+    get isNextDisabled() {
+      return props.currentPageItemsCount < props.itemsPerPage;
+    },
+  });
+
   return (
     <nav aria-label='Pagination Navigation'>
       <ul>
-        <li></li>
+        <li>
+          <Button
+            name='Prev'
+            variant='outline'
+            handleClick={props.handlePreviousClick}
+            disabled={state.isPreviousDisabled}
+            aria-label='Goto Previous Page'></Button>
+        </li>
+        <li>
+          <Button
+            name='Next'
+            variant='outline'
+            handleClick={props.handleNextClick}
+            disabled={state.isNextDisabled}
+            aria-label='Goto Next Page'></Button>
+        </li>
       </ul>
     </nav>
   );
