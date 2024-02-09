96b2639abc4248393cc66a9f7dbf49585cde2d9d
Kiran K <mailtokirankk@gmail.com>
2024-02-09 14:48:57 +0530

    Try tailwind button

--- internal/src/App.tsx
+++ internal/src/App.tsx
@@ -3,12 +3,9 @@ import { useState } from 'react';
 import './index.css';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <h1 className='text-1xl font-bold underline bg-red-400'>Hello world!</h1>
-      <button className='btn btn-primary'>Sign</button>
+      <button className='btn btn-primary btn-md'>Create Connection</button>
     </>
   );
 }
