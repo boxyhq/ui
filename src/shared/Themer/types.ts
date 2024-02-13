5848b750a3ee40c5df255edc08eb69bb6dc424b5
Deepak Prabhakara <deepak.prabhakara@gmail.com>
2024-02-13 05:53:29 +0000

    Tweaks/UI (#877)
    * fixed typo
    
    * tweaked Badge appearance
    
    * removed table border-radius, added dev script
    
    * wrap table columns to cater to unusually long non-breaking words (if present)
    
    * fixed version in dev script
    
    * Cleanup for Themer
    
    ---------
    
    Co-authored-by: Aswin V <vaswin91@gmail.com>

---
+++ src/shared/Themer/types.ts
@@ -0,0 +1,34 @@
+export type Theme = {
+  '--primary-color': string;
+  '--primary-foreground-color': string;
+  '--primary-hover': string;
+  '--primary-active': string;
+  '--primary-disabled': string;
+  '--primary-color-50': string;
+  '--primary-color-100': string;
+  '--primary-color-200': string;
+  '--primary-color-300': string;
+  '--primary-color-500': string;
+  '--primary-color-600': string;
+  '--primary-color-700': string;
+  '--primary-color-800': string;
+  '--primary-color-900': string;
+  '--primary-color-950': string;
+  '--secondary-color': string;
+  /* --secondary-color: "#27272a",  dark mode */
+  '--primary-text-color': string;
+  /* --primary-text-color: #fafafa, dark mode */
+  '--primary-text-color-500': string;
+  '--ring-offset': string;
+  '--ring-offset-width': string;
+  '--ring-offset-color': string;
+  /* --ring-offset-color: hsl(240 10 3.9),  dark mode*/
+  '--ring-color': string;
+  /* --ring-color: hsl(240 4.9% 83.9%),  dark mode */
+  '--border-color': string;
+  '--border-width': string;
+  '--border-radius': string;
+  '--alert-color-success': string;
+  '--alert-color-info': string;
+  '--alert-color-warning': string;
+};
