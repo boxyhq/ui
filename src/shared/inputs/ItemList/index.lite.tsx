5f4f71e1d130ac6119d515d0b29a2d096b5c1a60
Aswin V <vaswin91@gmail.com>
2024-03-29 14:23:16 +0530

    Use <for> control flow

--- src/shared/inputs/ItemList/index.lite.tsx
+++ src/shared/inputs/ItemList/index.lite.tsx
@@ -1,3 +1,4 @@
+import { For } from '@builder.io/mitosis';
 import ItemRow from './ItemRow.lite';
 
 export default function ItemList({
@@ -16,21 +17,23 @@ export default function ItemList({
   return (
     <div>
       <div className='flex flex-col gap-4'>
-        {list.map((item, index) => (
-          <div key={index}>
-            <ItemRow
-              item={item}
-              onItemChange={(newItem) => {
-                const newList = [...list];
-                newList[index] = newItem;
-                onItemListChange(newList);
-              }}
-              onItemDelete={() => {
-                onItemListChange(list.filter((_, i) => i !== index));
-              }}
-            />
-          </div>
-        ))}
+        <For each={list}>
+          {(item, index) => (
+            <div key={index}>
+              <ItemRow
+                item={item}
+                onItemChange={(newItem) => {
+                  const newList = [...list];
+                  newList[index] = newItem;
+                  onItemListChange(newList);
+                }}
+                onItemDelete={() => {
+                  onItemListChange(list.filter((_, i) => i !== index));
+                }}
+              />
+            </div>
+          )}
+        </For>
         <div>
           <button className='btn btn-primary btn-sm btn-outline' type='button' onClick={addAnother}>
             bui-fs-add
