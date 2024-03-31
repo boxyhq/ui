013527cbdf2fabd0e33711d940f8a760cd4c2ac3
Aswin V <vaswin91@gmail.com>
2024-04-01 00:19:22 +0530

    Label support, Display duplicate entry error inline, style tweaks, disable delete if only 1 item remains

--- src/shared/inputs/ItemList/ItemRow.lite.tsx
+++ src/shared/inputs/ItemList/ItemRow.lite.tsx
@@ -2,27 +2,34 @@ import XMarkIcon from '../../icons/XMarkIcon.lite';
 import styles from '../index.module.css';
 
 type ItemRowProps = {
+  inputType: 'text' | 'url' | 'number' | 'password';
   item: string;
   index: number;
   handleItemUpdate: (newItem: string, index: number) => void;
   handleItemDelete: (index: number) => void;
+  handleBlur: (index: number) => void;
+  isDuplicateItem?: boolean;
+  disableDelete?: boolean;
+  readOnly?: boolean;
 };
 
 export default function ItemRow(props: ItemRowProps) {
   return (
     <div className='flex space-x-3 items-center'>
       <input
-        type='text'
+        type={props.inputType || 'text'}
         className='input input-bordered input-sm w-full'
         name='item'
         value={props.item}
         onChange={(event) => props.handleItemUpdate(event.target.value, props.index)}
+        onBlur={(event) => props.handleBlur(props.index)}
         required
+        disabled={props.readOnly}
       />
       <button
         type='button'
         onClick={(event) => props.handleItemDelete(props.index)}
-        disabled={props.index === 0}>
+        disabled={props.disableDelete}>
         <XMarkIcon svgAttrs={{ class: styles['svg'] }} />
       </button>
     </div>
--- src/shared/inputs/ItemList/index.lite.tsx
+++ src/shared/inputs/ItemList/index.lite.tsx
@@ -1,15 +1,21 @@
-import { For, useStore } from '@builder.io/mitosis';
+import { For, Show, useStore } from '@builder.io/mitosis';
 import ItemRow from './ItemRow.lite';
+import styles from '../index.module.css';
+import listStyles from './index.module.css';
+import cssClassAssembler from '../../../sso/utils/cssClassAssembler';
 
 type ItemListProps = {
+  label: string;
+  inputType: 'text' | 'url' | 'number' | 'password';
+  classNames?: { label?: string };
   currentlist: string | string[];
   fieldName: string;
   handleItemListUpdate: (fieldName: string, newList: string[]) => void;
-  errorCallback?: (errorMessage: string) => void;
 };
 
 export default function ItemList(props: ItemListProps) {
   const state = useStore({
+    duplicateEntryIndex: undefined as undefined | number,
     get list() {
       return Array.isArray(props.currentlist) ? props.currentlist : [props.currentlist];
     },
@@ -18,35 +24,68 @@ export default function ItemList(props: ItemListProps) {
     },
     handleItemUpdate: (newItem: string, index: number) => {
       const newList = [...state.list];
-      const _index = newList.findIndex((item) => item === newItem);
-      if (_index !== -1 && _index !== index) {
-        typeof props.errorCallback === 'function' &&
-          props.errorCallback(`Duplicate item, ensure that the value is not already added`);
-      } else {
-        newList[index] = newItem;
-        props.handleItemListUpdate(props.fieldName, newList);
+      newList[index] = newItem;
+      props.handleItemListUpdate(props.fieldName, newList);
+    },
+    checkDuplicates(index: number) {
+      const _item = state.list[index];
+      // search backwards
+      const _firstIndex = state.list.indexOf(_item);
+      if (_firstIndex !== index) {
+        state.duplicateEntryIndex = index;
+        return;
+      } else if (state.duplicateEntryIndex === index) {
+        state.duplicateEntryIndex = undefined;
+      }
+      // search forwards
+      const _nextIndex = state.list.slice(index + 1).indexOf(_item);
+      if (_nextIndex !== -1) {
+        state.duplicateEntryIndex = index;
+      } else if (state.duplicateEntryIndex === index) {
+        state.duplicateEntryIndex = undefined;
       }
     },
     handleItemDelete: (index: number) => {
+      const _itemToDelete = state.list[index];
+      if (
+        state.duplicateEntryIndex !== undefined &&
+        (state.duplicateEntryIndex === index || _itemToDelete === state.list[state.duplicateEntryIndex])
+      ) {
+        state.duplicateEntryIndex = undefined;
+      }
       props.handleItemListUpdate(
         props.fieldName,
         state.list.filter((_, i) => i !== index)
       );
     },
+    get cssClass() {
+      return {
+        label: cssClassAssembler(props.classNames?.label, styles.label),
+      };
+    },
   });
 
   return (
-    <div>
-      <div className='flex flex-col gap-4'>
+    <fieldset class={styles.fieldset}>
+      <legend class={state.cssClass.label}>{props.label}</legend>
+      <div class={listStyles.rowContainer}>
         <For each={state.list}>
           {(item, index) => (
             <div>
               <ItemRow
+                inputType={props.inputType}
                 item={item}
                 index={index}
+                isDuplicateItem={state.duplicateEntryIndex === index}
                 handleItemUpdate={state.handleItemUpdate}
                 handleItemDelete={state.handleItemDelete}
+                disableDelete={index === 0 && state.list.length === 1}
+                handleBlur={state.checkDuplicates}
+                readOnly={state.duplicateEntryIndex !== undefined && state.duplicateEntryIndex !== index}
               />
+              <Show when={state.duplicateEntryIndex === index}>
+                <span>Duplicate entries not allowed</span>
+              </Show>
             </div>
           )}
         </For>
@@ -54,11 +93,12 @@ export default function ItemList(props: ItemListProps) {
           <button
             className='btn btn-primary btn-sm btn-outline'
             type='button'
+            disabled={state.duplicateEntryIndex !== undefined}
             onClick={(event) => state.addAnother()}>
-            Add
+            Add URL
           </button>
         </div>
       </div>
-    </div>
+    </fieldset>
   );
 }
