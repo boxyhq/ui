214dac3c7f84f39dcd5e21730af7835f41595bf6
Aswin V <vaswin91@gmail.com>
2024-04-01 22:52:23 +0530

    Export `ItemList` for external use

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,3 +1,4 @@
 export { default as ClipboardButton } from './ClipboardButton/index.lite';
 export { default as InputWithCopyButton } from './inputs/InputWithCopyButton/index.lite';
 export { default as Themer } from './Themer/index.lite';
+export { default as ItemList } from './inputs/ItemList/index.lite';
